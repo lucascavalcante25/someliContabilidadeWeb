a2f35e3f97977d2ed208afb4a2204472a01cef4a
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-06-22 00:29:39 -0300

    ajusta layout e regra da tela de dashboard despesas do escritorio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://somelicontabilidadeback.onrender.com'
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: 'https://somelicontabilidadeback.onrender.com'
+  // apiBaseUrl: 'http://localhost:8080',
 
 };
 
